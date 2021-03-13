89070679337e59700534722883b7c086162b0db7
Nur Mahabub Morshedur Rahman <nrahman171291@bscse.uiu.ac.bd>
2021-03-13 10:14:49 +0600

    Added "viewall.php"

---
+++ viewall.php
@@ -0,0 +1,35 @@
+<?php
+//Including the database connection file
+include_once("database.php");
+
+//Fetching data in descending order (latest entry first)
+$result = mysqli_query($connection, "SELECT * FROM appointment ORDER BY ID DESC");
+?>
+
+    <html>
+<head>
+    <title>Appointment List</title>
+</head>
+
+<body>
+<a href="add.html">Add New Appointment</a><br><a href="search.html">Search for Appointments</a><br/><br/>
+
+<table width='80%' border=0>
+    <tr bgcolor='#CCCCCC'>
+        <td>Doctor's Name</td>
+        <td>Patient's Name</td>
+        <td>Appointment Date</td>
+        <td>Update</td>
+    </tr>
+    <?php
+    while ($res = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td>" . $res['DOCTOR_NAME'] . "</td>";
+        echo "<td>" . $res['PATIENT_NAME'] . "</td>";
+        echo "<td>" . $res['DATE'] . "</td>";
+        echo "<td><a href=\"edit.php?id=$res[ID]\">Edit</a> | <a href=\"delete.php?id=$res[ID]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+    }
+    ?>
+</table>
+</body>
+    </html><?php
